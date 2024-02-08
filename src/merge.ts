94021e13efcd87cc133dc1404336f11559753141
Awat Singdamrong <a.lubing@yahoo.com>
2024-02-08 19:54:16 +0700

    feat: merge function

--- src/merge.ts
+++ src/merge.ts
@@ -1,3 +1,74 @@
-export function add(x: number, y: number): number {
-    return x + y;
+export default function merge(collection_1: number[], collection_2: number[], collection_3: number[]): number[] {
+    // given
+    //  collection_1, collection_2 already sorted from min(0) to max
+    //  collection_3  already sorted from max to min(0)
+    let i = 0;
+    let j = 0;
+    let k = collection_3.length - 1;
+
+    const col1 = collection_1.length;
+    const col2 = collection_2.length;
+
+    let ans: number[] = [];
+
+    while (i < col1 && j < col2 && k >= 0) {
+        let m = Math.min(Math.min(collection_1[i], collection_2[j]), collection_3[k]);
+        ans.push(m);
+        if (m == collection_1[i]) i++;
+        else if (m == collection_2[j]) j++;
+        else k--;
+    }
+
+    // col1 is out
+    while (j < col2 && k >= 0) {
+        if (collection_2[j] <= collection_3[k]) {
+            ans.push(collection_2[j]);
+            j++;
+        } else {
+            ans.push(collection_3[k]);
+            k--;
+        }
+    }
+
+    // col2 is out
+    while (i < col1 && k >= 0) {
+        if (collection_1[i] <= collection_3[k]) {
+            ans.push(collection_1[i]);
+            i++;
+        } else {
+            ans.push(collection_3[k]);
+            k--;
+        }
+    }
+
+    // col3 is out
+    while (i < col1 && j < col2) {
+        if (collection_1[i] <= collection_2[j]) {
+            ans.push(collection_1[i]);
+            i++;
+        } else {
+            ans.push(collection_2[j]);
+            j++;
+        }
+    }
+
+    // col1, col2 is out
+    while (k >= 0) {
+        ans.push(collection_3[k]);
+        k--;
+    }
+
+    // col1, col3 is out
+    while (j < col2) {
+        ans.push(collection_2[j]);
+        j++;
+    }
+
+    // col2, col3 is out
+    while (i < col1) {
+        ans.push(collection_1[i]);
+        i++;
+    }
+
+    return ans;
 }
--- test/merge.test.ts
+++ test/merge.test.ts
@@ -1,9 +1,94 @@
-import { add } from "../src/merge";
-describe("test add function", () => {
-    it("should return 15 for add(10,5)", () => {
-        expect(add(10, 5)).toBe(15);
+import merge from "../src/merge";
+
+describe("test merge function", () => {
+    it("case 1: All collections have same length", () => {
+        const collection_1 = [1, 4, 7, 9];
+        const collection_2 = [2, 3, 5, 10];
+        const collection_3 = [12, 11, 8, 6];
+
+        const mergeResult = merge(collection_1, collection_2, collection_3);
+        const ans = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+        expect(mergeResult).toStrictEqual(ans);
     });
-    it("should return 5 for add(2,3)", () => {
-        expect(add(2, 3)).toBe(5);
+    it("case 2: collection_1 has different length", () => {
+        const collection_1 = [1, 4, 7, 9, 13];
+        const collection_2 = [2, 3, 5, 10];
+        const collection_3 = [12, 11, 8, 6];
+
+        const mergeResult = merge(collection_1, collection_2, collection_3);
+        const ans = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+        expect(mergeResult).toStrictEqual(ans);
+    });
+    it("case 3: collection_2 has different length", () => {
+        const collection_1 = [1, 4, 7, 9];
+        const collection_2 = [2, 3, 5, 10, 13];
+        const collection_3 = [12, 11, 8, 6];
+
+        const mergeResult = merge(collection_1, collection_2, collection_3);
+        const ans = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+        expect(mergeResult).toStrictEqual(ans);
+    });
+    it("case 4: collection_3 has different length", () => {
+        const collection_1 = [1, 4, 7, 9];
+        const collection_2 = [2, 3, 5, 10];
+        const collection_3 = [13, 12, 11, 8, 6];
+
+        const mergeResult = merge(collection_1, collection_2, collection_3);
+        const ans = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+        expect(mergeResult).toStrictEqual(ans);
+    });
+    it("case 5: All collections have different length", () => {
+        const collection_1 = [1, 4, 7];
+        const collection_2 = [2, 3, 5, 9];
+        const collection_3 = [12, 11, 10, 8, 6];
+
+        const mergeResult = merge(collection_1, collection_2, collection_3);
+        const ans = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+        expect(mergeResult).toStrictEqual(ans);
+    });
+    it("case 6: collection_1 is empty", () => {
+        const collection_1: number[] = [];
+        const collection_2 = [1, 3, 5, 7];
+        const collection_3 = [8, 6, 4, 2];
+
+        const mergeResult = merge(collection_1, collection_2, collection_3);
+        const ans = [1, 2, 3, 4, 5, 6, 7, 8];
+
+        expect(mergeResult).toStrictEqual(ans);
+    });
+    it("case 7: collection_2 is empty", () => {
+        const collection_1 = [1, 3, 5, 7];
+        const collection_2: number[] = [];
+        const collection_3 = [8, 6, 4, 2];
+
+        const mergeResult = merge(collection_1, collection_2, collection_3);
+        const ans = [1, 2, 3, 4, 5, 6, 7, 8];
+
+        expect(mergeResult).toStrictEqual(ans);
+    });
+    it("case 8: collection_3 is empty", () => {
+        const collection_1 = [1, 3, 5, 7];
+        const collection_2 = [2, 4, 6, 8];
+        const collection_3: number[] = [];
+
+        const mergeResult = merge(collection_1, collection_2, collection_3);
+        const ans = [1, 2, 3, 4, 5, 6, 7, 8];
+
+        expect(mergeResult).toStrictEqual(ans);
+    });
+    it("case 9: Mix with negative number", () => {
+        const collection_1 = [-1, 1, 5];
+        const collection_2 = [-3, -2, 3, 6];
+        const collection_3 = [7, 4, 2, 0, -4];
+
+        const mergeResult = merge(collection_1, collection_2, collection_3);
+        const ans = [-4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7];
+
+        expect(mergeResult).toStrictEqual(ans);
     });
 });
